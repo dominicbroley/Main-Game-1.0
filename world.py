9b1e360ea8f9a3866fe3c6bd413b5d0310c665e5
dominicbroley <71086499+dominicbroley@users.noreply.github.com>
2021-04-30 22:46:40 -0600

    Update world.py

--- world.py
+++ world.py
@@ -1,6 +1,14 @@
+# Name: Dominic Broley
+# Class: CS30 Quint 4
+# Date: April 29, 2021
+# Description: Game
+# Adapted code from https://github.com/kynite/FishingRPG
+
+
 from player import Player
 
-player = Player(1, 2)
+player = Player(1, 1)
+
 
 class MapTile:
     """Parent Class to child classes"""
@@ -15,20 +23,6 @@ class MapTile:
         return self.name
 
 
-class mountain(MapTile):
-    """Location with axe and apples"""
-    def __init__(self, x, y):
-        self.name = "mountain"
-        # Position of the mountain tile
-        super().__init__(x, y)
-
-class farm(MapTile):
-    """Location with axe and apples"""
-    def __init__(self, x, y):
-        self.name = "farm"
-        # Position of the farm tile
-        super().__init__(x, y)
-
 class forest(MapTile):
     """Location with ninja and chicken"""
     def __init__(self, x, y):
@@ -36,12 +30,6 @@ class forest(MapTile):
         # Position of the forest tile
         super().__init__(x, y)
 
-class cave(MapTile):
-    """Location with field"""
-    def __init__(self, x, y):
-        self.name = "cave"
-        # Position of the cave tile
-        super().__init__(x, y)
 
 class mountain(MapTile):
     """Location with zombie and steak"""
@@ -50,6 +38,7 @@ class mountain(MapTile):
         # Position of the mountain tile
         super().__init__(x, y)
 
+
 class farm(MapTile):
     """Location with apple and axe"""
     def __init__(self, x, y):
@@ -57,12 +46,22 @@ class farm(MapTile):
         # Position of the farm tile
         super().__init__(x, y)
 
+
 class cave(MapTile):
     """Location with a skeleton and zombie"""
     def __init__(self, x, y):
         self.name = "cave"
         # Position of the cave tile
         super().__init__(x, y)
+
+
+class barrier(MapTile):
+    """Location with the end"""
+    def __init__(self, x, y):
+        self.name = "barrier"
+        # Position of the barrier tile
+        super().__init__(x, y)
+
 
 class end(MapTile):
     """Location with the end"""
@@ -73,6 +72,12 @@ class end(MapTile):
 
 
 # The World Map
-world_map = [[Player, mountain(1,0), farm(2,0)],
-            [forest(0, 1), cave(1, 1), mountain(2,1)],
-            [farm(0,2), cave(2, 2), end(2,2)]]
+world_map = [[barrier(0, 0), barrier(1, 0), barrier(2, 0)],
+             [barrier(3, 0), barrier(4, 0)],
+             [barrier(0, 1), player, None, cave(3, 1), barrier(4, 1)],
+             [barrier(0, 2), forest(1, 2), None],
+             [mountain(3, 2), barrier(4, 2)],
+             [barrier(0, 3), farm(1, 3), None],
+             [end(3, 3), barrier(4, 3)],
+             [barrier(0, 4), barrier(1, 4), barrier(2, 4)],
+             [barrier(3, 4), barrier(4, 4)]]
